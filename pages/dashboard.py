852ce4447759a3a8e637df2582244c98ecf91a93
kornkek <122936250+kornkek@users.noreply.github.com>
2023-01-24 02:24:04 +0300

    Create dashboard.py

---
+++ pages/dashboard.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    main_page_button_xpath = "//*[@id='__next']//ul/div"
+    players_button_xpath = "//*[@id='__next']//ul/div[2]"
+    lang_button_xpath = "//*[@id='__next']//ul[2]/div"
+    sign_out_button_xpath = "//*[@id='__next']//ul[2]/div[2]"
+    dev_team_button_xpath = "//*[@id='__next']//div[3]//div/div[3]/a"
+    add_player_button_xpath = "//*[@id='__next']//div[3]/div[2]/div//a/button"
+    
+
+
