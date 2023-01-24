a9dda97f5ca1a4e0eef9afb00f4ee8884aa3260b
kornkek <122936250+kornkek@users.noreply.github.com>
2023-01-24 23:39:55 +0300

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,6 +8,9 @@ class Dashboard(BasePage):
     sign_out_button_xpath = "//*[@id='__next']//ul[2]/div[2]"
     dev_team_button_xpath = "//*[@id='__next']//div[3]//div/div[3]/a"
     add_player_button_xpath = "//*[@id='__next']//div[3]/div[2]/div//a/button"
-    
+    players_count_table_xpath = "//*[@id='__next']//div[2]/div/div"
+    matches_count_table_xpath = "//*[@id='__next']//div[2]/div[2]/div"
+    reports_count_table_xpath = "//*[@id='__next']//div[2]/div[2]/div"
+    events_count_table_xpath = "//*[@id='__next']//div[2]/div[4]/div"
 
 
